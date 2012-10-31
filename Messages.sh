8382a9bc6cbea12f9c8b18d9c4b82b1de4b96cb0
Daniel Calviño Sánchez <danxuliu@gmail.com>
2012-10-31 12:31:33 +0100

    Add missing extension for message extraction.
    The .kcfg extension is used for configuration files.

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$EXTRACTRC `find . -maxdepth 1 -name \*.rc -o -name \*.ui` `find settings -name \*.rc -o -name \*.ui` >> rc.cpp
+$EXTRACTRC `find . -maxdepth 1 -name \*.rc -o -name \*.ui -o -name \*.kcfg` `find settings -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -maxdepth 1 -name \*.cc -o -name \*.cpp -o -name \*.h` `find settings -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kdevkrazy2.pot
 rm -f rc.cpp
